ea608fd7629bcd224b4766edc3bcd85d11f89d74
Fish_In_A_Suit <skorjanc.aljosa@gmail.com>
2022-10-21 14:24:34 +0200

    0.2: Added GO Terms for search term "angiogenesis"
    Searched for "angiogenesis" in AmiGO2 (http://amigo.geneontology.org/amigo/search/ontology?q=angiogenesis) and transferred the GO Terms with their associated GO-id's to constants.py file.
    
    Note: only terms from idspace: GO were added.

---
+++ constants.py
@@ -0,0 +1,54 @@
+# Includes constant values, arrays, etc
+
+# Positive regulatory terms for search "angiogenesis"
+TERMS_ANGIOGENESIS_POSITIVE_ARRAY = [
+    "positive regulation of blood vessel endothelial cell proliferation involved in sprouting angiogenesis", "GO:1903589",
+    "positive regulation of sprouting angiogenesis", "GO:1903672",
+    "positive regulation of angiogenesis", "GO:0045766",
+    "positive regulation of cell adhesion involved in sprouting angiogenesis", "GO:0106090",
+    "positive regulation of cell migration involved in sprouting angiogenesis", "GO:0090050"
+    ]
+
+# Negative regulatory terms for search "angiogenesis"
+TERMS_ANGIOGENESIS_NEGATIVE_ARRAY = [
+    "negative regulation of blood vessel endothelial cell proliferation involved in sprouting angiogenesis", "GO:1903588",
+    "negative regulation of sprouting angiogenesis", "GO:1903671",
+    "negative regulation of angiogenesis", "GO:0016525",
+    "negative regulation of cell adhesion involved in sprouting angiogenesis", "GO:0106089",
+    "negative regulation of cell migration involved in sprouting angiogenesis", "GO:0090051",
+    "angiostatin binding", "GO:0043532",
+    "angiogenin-PRI complex", "GO:0032311"
+    ]
+
+# Positive regulatory terms for search "diabetes"
+TERMS_DIABETES_POSITIVE_ARRAY = []
+
+# Negative regulatory terms for search "diabetes"
+TERMS_DIABETES_NEGATIVE_ARRAY = []
+
+# Terms that are neither positive or negative
+TERMS_GENERAL = [
+    "angiogenesis", "GO:0001525",
+    "regulation of blood vessel endothelial cell proliferation involved in sprouting angiogenesis", "GO:1903587",
+    "regulation of sprouting angiogenesis", "GO:1903670",
+    "regulation of angiogenesis", "GO:0045765",
+    "angiogenesis involved in wound healing", "GO:0060055",
+    "sprouting angiogenesis", "GO:0002040",
+    "intussusceptive angiogenesis", "GO:0002041",
+    "cell migration involved in sprouting angiogenesis", "GO:0002042",
+    "blood vessel endothelial cell proliferation involved in sprouting angiogenesis", "GO:0002043",
+    "blood vessel endothelial cell migration involved in intussusceptive angiogenesis", "GO:0002044",
+    "regulation of cell adhesion involved in sprouting angiogenesis", "GO:0106088",
+    "regulation of cell adhesion involved in intussusceptive angiogenesis", "GO:0002045",
+    "cell adhesion involved in sprouting angiogenesis", "GO:0120078",
+    "regulation of cell migration involved in sprouting angiogenesis", "GO:0090049",
+    "alpha6-beta1 integrin-CYR61 complex", "GO:0071116",
+    "angiogenic sprout fusion", "GO:0120077",
+    "blood vessel endothelial cell migration", "GO:0043534"
+]
+
+# Terms that came up when searching "angiogenesis", but are irrelevant for us
+TERMS_EXCLUDED = [
+    "cell migration involved in coronary angiogenesis", "GO:0060981",
+    "angiogenesis involved in coronary vascular morphogenesis", "GO:0060978",
+]
--- util.py
+++ util.py
@@ -1,5 +1,5 @@
 # Utility functions file
 import requests
 
-x = requests.get("")
-print(x.text)
+#x = requests.get("http://amigo.geneontology.org/amigo/search/ontology?q=angiogenesis")
+#print(x.text)
