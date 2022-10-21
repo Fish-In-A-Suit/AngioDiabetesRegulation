8e4495bd21e2be9246d42c189c6491c15ae73126
Fish_In_A_Suit <skorjanc.aljosa@gmail.com>
2022-10-21 13:22:54 +0200

    0.1: Introduction
    main.py file, where the main script is found.
    
    util.py file for utility functions

---
+++ main.py
@@ -0,0 +1,24 @@
+# Main script file
+
+"""
+Introduction to Gene Ontology:
+    - GO Annotation = a statement about the function of a particular gene (created by associating
+      a gene / gene product with a GO Term). It includes statements about
+         (1) how the gene functions at the molecular level
+         (2) where in the cell it functions and
+         (3) what biological pathways it helps carry out.
+      At the minimum, an annotation consists of:
+         (1) Gene product (may be a protein, RNA, etc)
+         (2) GO term
+         (3) Reference
+         (4) Evidence (go evidence codes: http://geneontology.org/docs/guide-go-evidence-codes/)
+      Additionally, annotations can be equipped with 'annotation qualifiers': NOT, contributes_to, colocalizes_with
+"""
+
+     
+
+
+
+
+
+
---
+++ util.py
@@ -0,0 +1,5 @@
+# Utility functions file
+import requests
+
+x = requests.get("")
+print(x.text)
