7a62ef98bb28aa13876519d903ac3ef47e8fe4d7
ladismrkolj <ladismrkolj@gmail.com>
2022-10-22 21:55:26 +0200

    add test code

--- dev_test_api_download.py
+++ dev_test_api_download.py
@@ -1,9 +1,12 @@
 #File to test the API get function to retrieve genes for
 #terms in constants.py
+from re import S
+from xmlrpc.client import ResponseError
 import requests
 import urllib.parse
 import json
 import time
+import csv
 
 import logging
 
@@ -21,63 +24,85 @@ logging.basicConfig(
 )
 
 
-def get_GO_genes_API(termlist):
+def get_GO_genes_API(term):
     """
     This function retrieves all genes associated with each term.
     Input of GO terms must be a 1d list of GO Term Accession. e.g. ['GO:1903502','GO:1903508'].
     """
-    for term in termlist:
-        url_term = urllib.parse.quote(term)
-        parameters = {
-            "use_compact_associations":True,
-            "taxon":["NCBITaxon:9606"] #only for Homo Sapiens
-        }
-        response = requests.get(f"http://api.geneontology.org/api/bioentity/function/{url_term}/genes", params=parameters)
-        logging.debug(json.dumps(response.json()['compact_associations'], indent=4))
-        compact_assoc = response.json()['compact_associations']
-        for item in compact_assoc:
-            if item['subject'] == term: #only use directly associated genes
-                genes=item['objects']
+    url_term = urllib.parse.quote(term)
+    parameters = {
+        "use_compact_associations":True,
+        "taxon":["NCBITaxon:9606"] #only for Homo Sapiens
+    }
+    response = requests.get(f"http://api.geneontology.org/api/bioentity/function/{url_term}/genes", params=parameters)
+    logging.debug(json.dumps(response.json()['compact_associations'], indent=4))
+    compact_assoc = response.json()['compact_associations']
+    for item in compact_assoc:
+        if item['subject'] == term: #only use directly associated genes
+            genes=item['objects']
             logging.info(f"GO term: {term} -> Genes/products: {genes}")
+    return genes
 
-def get_ensembl_sequence_API(ID_list):
+def get_ensembl_sequence_API(id):
     """
     This function queries ensembl for nucleotide sequence
     Input of ensembl ID's must be a 1d list. e.g. ['ENSG00000157764']
     """
-    for id in ID_list:
-        response = requests.get(f"https://rest.ensembl.org/sequence/id/{id}", headers={ "Content-Type" : "text/plain"})
+    response = requests.get(f"https://rest.ensembl.org/sequence/id/{id}", headers={ "Content-Type" : "text/plain"})
+    if response.ok:
         logging.debug(response.text)
+        logging.info(f"Recieved sequence for id {id}.")
+        return response.text
+    else:
+        return None
 
-def uniprot_mapping(id_list, target='Ensembl'):
+def uniprot_mapping(id_old, target='Ensembl'):
     """
     This function recieves uniprot or other ID and finds the Ensembl id.
     Input of ID's must be a 1d list. e.g. ['UniProtKB:A0A3Q1N508']
-    """
-
-    for item in id_list:
-        if "UniProtKB" in item:
-            source = "UniProtKB_AC-ID"
-        id = item.split(':')[1]
-        response = requests.post(f"https://rest.uniprot.org/idmapping/run", data={'from':source, 'to':target, 'ids':id})
-        logging.debug(response.json())
-        jobId=response.json()['jobId']
-        logging.debug(jobId)
-        POLLING_INTERVAL=1
-        while True:
-            request = requests.get(f"https://rest.uniprot.org/idmapping/status/{jobId}")
-            j = request.json()
-            logging.info(j)
-            if "jobStatus" in j:
-                if j["jobStatus"] == "RUNNING":
-                    print(f"Retrying in {POLLING_INTERVAL}s")
-                    time.sleep(POLLING_INTERVAL)
-                else:
-                    raise Exception(j["jobStatus"])
+"""
+    if "UniProtKB" in id_old:
+        source = "UniProtKB_AC-ID"
+    id = id_old.split(':')[1]
+    response = requests.post(f"https://rest.uniprot.org/idmapping/run", data={'from':source, 'to':target, 'ids':id})
+    logging.debug(response.json())
+    jobId=response.json()['jobId']
+    logging.debug(jobId)
+    POLLING_INTERVAL=1
+    while True:
+        request = requests.get(f"https://rest.uniprot.org/idmapping/status/{jobId}")
+        j = request.json()
+        logging.info(j)
+        if "jobStatus" in j:
+            if j["jobStatus"] == "RUNNING":
+                print(f"Retrying in {POLLING_INTERVAL}s")
+                time.sleep(POLLING_INTERVAL)
             else:
-                return bool(j["results"] or j["failedIds"])
+                raise Exception(j["jobStatus"])
+        else:
+            if 'failedIds' in j:
+                ensembl_id=None
+            else:
+                ensembl_id=j['results'][0]['to'].split('.')[0]
+            break
+    logging.info(ensembl_id)
+    return ensembl_id
         
-
-#get_GO_genes_API(['GO:0001525'])
-#get_ensembl_sequence_API(["ENSG00000157764"])
-uniprot_mapping(['UniProtKB:A0A3Q1N508'])
+        
+#DEMO TEST CODE, not to be used in production
+f = open("demofile2.txt", "w+")
+terms = ['GO:0001525']
+for term in terms:
+    genes = get_GO_genes_API(term)
+    e_id = []
+    seqeunces = []
+    print(len(genes))
+    for i in range(len(genes)):
+        e_id.append(uniprot_mapping(genes[i]))
+        if e_id[i] == None:
+            seqeunces.append(None)
+        else:
+            seqeunces.append(get_ensembl_sequence_API(e_id[i]))
+        out = {"term" : term, "gene" : genes[i], "ensembel_id" : e_id[i], "sequence" : seqeunces[i]}
+        f.write(json.dumps(out)+"\n")
+f.close()
