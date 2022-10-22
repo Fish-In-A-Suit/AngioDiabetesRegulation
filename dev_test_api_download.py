3d21827ef40c69e12bd04654c0354326e4fc4489
ladismrkolj <ladismrkolj@gmail.com>
2022-10-22 18:45:11 +0200

    add initial uniprot id mapping

--- dev_test_api_download.py
+++ dev_test_api_download.py
@@ -3,15 +3,20 @@
 import requests
 import urllib.parse
 import json
+import time
 
 import logging
 
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+file_handler = logging.FileHandler("./log_output/test_json_dump.log", 'w+')
+file_handler.setLevel(logging.DEBUG)
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("./log_output/test_json_dump.log", 'w+'),
-        logging.StreamHandler()
+        file_handler,
+        console_handler
     ]
 )
 
@@ -44,5 +49,35 @@ def get_ensembl_sequence_API(ID_list):
         response = requests.get(f"https://rest.ensembl.org/sequence/id/{id}", headers={ "Content-Type" : "text/plain"})
         logging.debug(response.text)
 
+def uniprot_mapping(id_list, target='Ensembl'):
+    """
+    This function recieves uniprot or other ID and finds the Ensembl id.
+    Input of ID's must be a 1d list. e.g. ['UniProtKB:A0A3Q1N508']
+    """
 
-get_ensembl_sequence_API(["ENSG00000157764"])
+    for item in id_list:
+        if "UniProtKB" in item:
+            source = "UniProtKB_AC-ID"
+        id = item.split(':')[1]
+        response = requests.post(f"https://rest.uniprot.org/idmapping/run", data={'from':source, 'to':target, 'ids':id})
+        logging.debug(response.json())
+        jobId=response.json()['jobId']
+        logging.debug(jobId)
+        POLLING_INTERVAL=1
+        while True:
+            request = requests.get(f"https://rest.uniprot.org/idmapping/status/{jobId}")
+            j = request.json()
+            logging.info(j)
+            if "jobStatus" in j:
+                if j["jobStatus"] == "RUNNING":
+                    print(f"Retrying in {POLLING_INTERVAL}s")
+                    time.sleep(POLLING_INTERVAL)
+                else:
+                    raise Exception(j["jobStatus"])
+            else:
+                return bool(j["results"] or j["failedIds"])
+        
+
+#get_GO_genes_API(['GO:0001525'])
+#get_ensembl_sequence_API(["ENSG00000157764"])
+uniprot_mapping(['UniProtKB:A0A3Q1N508'])
