ce107d7da26507ec32279a11dbf64c247a6c2c0d
Fish_In_A_Suit <skorjanc.aljosa@gmail.com>
2023-04-12 17:52:05 +0200

    Analyze unique saves in ReverseLookup
    Method analyze_saves checks the amount of unique json data files for a model with a particular name. This is the basis towards a hash- or timestamp-based model data saving in the future.

--- constants.py
+++ constants.py
@@ -6,7 +6,8 @@
 #
 #
 
-TARGET_FOLDER = "diabetes_angio_1" #just a constant to be used in all stages of analysis
+# TARGET_FOLDER = "diabetes_angio_1" #just a constant to be used in all stages of analysis
+TARGET_FOLDER = "diabetes_angio_1"
 
 FILE_miRDB_miRNAs = "src_data_files/miRNAdbs/miRDB_v6.0_prediction_result.txt"
 
