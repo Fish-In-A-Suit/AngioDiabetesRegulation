ee7e9d83b14e62033e5febb999407e7c759db163
Fish_In_A_Suit <37004357+Fish-In-A-Suit@users.noreply.github.com>
2023-08-15 23:37:03 +0200

    Reorganised code

--- test_playground.py
+++ test_playground.py
@@ -1,3 +1,5 @@
+# Testing file
+
 from goreverselookuplib import ReverseLookup
 from goreverselookuplib.AnnotationProcessor import GOAnnotiationsFile
 from goreverselookuplib.Workflows import WorkflowOne
@@ -7,7 +9,7 @@ import aiohttp
 from goreverselookuplib.JsonUtil import SimpleNamespaceUtil, JsonToClass
 from goreverselookuplib.AnnotationProcessor import HumanOrthologFinder, UniProtAPI, EnsemblAPI, GOAnnotiationsFile
 from goreverselookuplib.CacheUtils import ConnectionCacher, Cacher
-from goreverselookuplib.Metrics import fisher_exact_test
+from goreverselookuplib.Metrics import fisher_exact_test, adv_product_score, nterms, binomial_test
 
 import logging
 
@@ -15,122 +17,9 @@ from math import log10
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(funcName)s: %(message)s')
-
 # Create a file handler
 file_handler = logging.FileHandler('./log_output/test_json_dump.log')
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(logging.Formatter('%(asctime)s [%(filename)s:%(lineno)s] - [%(funcName)s] %(message)s'))
-
 logger = logging.getLogger(__name__)
-
-# before 06.06.2023
-# model = ReverseLookup.load_model("diabetes_angio_2/data_web_full_scores.json")
-# 06.06.2023: new code to perform statistical analysis on the model
-# model = ReverseLookup.load_model("diabetes_angio_2/data_06-06-2023.json")
-# model.perform_statistical_analysis(filepath="diabetes_angio_2/stat_analysis_06-06-2023.json")
-# exit()
-
-#test execution speed
-#import timeit
-#goaf = GOAnnotiationsFile()
-#elapsed_time = timeit.timeit(lambda: goaf.get_products("GO:1903589"), number=100)
-#print(elapsed_time)
-
-#goaf = GOAnnotiationsFile()
-#print(goaf.get_all_terms_for_product("ACAT2"))
-#print(len(goaf.get_all_terms()))
-
-
-""" # Ladi's code to perform statistical analysis for the products
-if False:
-
-    out_string = ""
-
-    for p in model.products:
-        entry = p.genename
-        for pr in model.target_processes:
-            for d in ['+','-']:
-                entry = entry + "\t" + f"{-log10(p.scores['fisher_test'][pr['process']+d]['pvalue_corr']):.2e}"
-        out_string = out_string + entry + "\n"
-
-    with open('out.txt', 'w') as f:
-        f.write(out_string)
-
-
-    exit()
-
-if True:
-    model.pre
-
-    for product in model.products:
-        #processes = model.target_processes
-        processes = [     
-            {
-                "process": "diabetes",
-                "direction": "+"
-            },
-            {
-                "process": "angio",
-                "direction": "+"
-            },
-            {
-                "process": "angio",
-                "direction": "+"
-            },
-            #{
-            #    "process": "obesity",
-            #    "direction": "+"
-            #}
-            ]
-        
-    #    if (all(float(product.scores["fisher_test"][f"{process['process']}{process['direction']}"]["pvalue_corr"]) < 0.05 for process in processes)):
-    #        print(f"Found significant product: {product.genename}")
-
-        if "fisher_test" in product.scores:
-            if (all(float(product.scores["fisher_test"][f"{process['process']}{process['direction']}"].get("pvalue_corr", 1)) < 0.05 for process in processes) and
-                all(float(product.scores["fisher_test"][f"{process['process']}{'+' if process['direction'] == '-' else '-'}"].get("pvalue_corr", 1)) >= 0.05 for process in processes)):
-                print(f"Found significant product: {product.genename}")
-    exit()
-model.prune_products()
-"""
-
-reference_model = ReverseLookup.load_model("diabetes_angio_2/data_06-06-2023.json")
-src_model = ReverseLookup.from_input_file("diabetes_angio_4/input.txt")
-api = GOApi()
-
-### Async ortholog query testing
-# load the existing model
-# model_async = ReverseLookup.from_input_file("diabetes_angio_4/input.txt") # use this to work from the ground up
-model_async = ReverseLookup.load_model("diabetes_angio_4/model_async_post_ortholog.json") # or use a pre-computed async model
-# Cacher.init()
-# ConnectionCacher.init()
-
-# if products for GO Terms have not been computed and saved yet, compute them:
-# model_async.fetch_all_go_term_products(web_download=True, run_async=True, recalculate=False, delay=0.0, run_async_options="v3", request_params={"rows":50000}, max_connections=60)
-# model_async.create_products_from_goterms()
-# model_async.save_model("diabetes_angio_4/model_async_test.json")
-
-# ortholog product fetch
-# model_async.fetch_ortholog_products(refetch=True, run_async=True, max_connections=15, req_delay=0.1, semaphore_connections=5) # semaphore_connections=10 works in 3min40s, semaphore_connections=15 results in 429:TooManyRequests errors
-# model_async.save_model("diabetes_angio_4/model_async_post_ortholog.json")
-
-# test model.fetch_product_infos
-# model_async.fetch_product_infos(refetch=True, run_async=True, max_connections=15, semaphore_connections=10, req_delay=0.1)
-# model_async.save_model("diabetes_angio_4/model_async_post_infos.json")
-
-# test model scoring
-model_async_queried = ReverseLookup.load_model("diabetes_angio_4/model_async_post_infos.json")
-goaf = GOAnnotiationsFile()
-fisher_test = fisher_exact_test(model_async_queried, goaf)
-model_async_queried.score_products(score_classes=[fisher_test])
-model_async_queried.save_model("diabetes_angio_4/model_async_scored.json")
-
-# test model statistical analysis
-model_async_scored = ReverseLookup.load_model("diabetes_angio_4/model_async_scored.json")
-model_async_scored.perform_statistical_analysis(test_name="fisher_test", filepath="diabetes_angio_4/statistical_analysis.json")
-model_async_scored.save_model("diabetes_angio_4/model_async_scored.json")
-
-
-
-
 
---
+++ test_playground_model.py
@@ -0,0 +1,61 @@
+# This file showcases the entire program usage as per the latest version
+# (with async queries).
+
+from goreverselookuplib import ReverseLookup
+from goreverselookuplib.AnnotationProcessor import GOAnnotiationsFile
+from goreverselookuplib.Workflows import WorkflowOne
+from goreverselookuplib.AnnotationProcessor import GOApi
+import asyncio
+import aiohttp
+from goreverselookuplib.JsonUtil import SimpleNamespaceUtil, JsonToClass
+from goreverselookuplib.AnnotationProcessor import HumanOrthologFinder, UniProtAPI, EnsemblAPI, GOAnnotiationsFile
+from goreverselookuplib.CacheUtils import ConnectionCacher, Cacher
+from goreverselookuplib.Metrics import fisher_exact_test, adv_product_score, nterms, binomial_test
+
+import logging
+
+from math import log10
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(funcName)s: %(message)s')
+# Create a file handler
+file_handler = logging.FileHandler('./log_output/test_json_dump.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter('%(asctime)s [%(filename)s:%(lineno)s] - [%(funcName)s] %(message)s'))
+logger = logging.getLogger(__name__)
+
+# Init Cacher for async queries
+Cacher.init()
+
+# Load model from input file
+model = ReverseLookup.from_input_file("diabetes_angio_4/input.txt")
+# or load pre-computed model: model = ReverseLookup.load_model("diabetes_angio_2/data_06-06-2023.json")
+
+# Fetch all GO term names and descriptions
+model.fetch_all_go_term_names_descriptions(run_async=True)
+# Fetch all GO term products
+model.fetch_all_go_term_products(web_download=True, run_async=True, recalculate=False, max_connections = 60, request_params={"rows":50000}, delay = 0.0)
+# Create product instances from GO terms
+model.create_products_from_goterms()
+# Fetch human ortholog for products (either UniProtID, ENSG or genename)
+model.fetch_ortholog_products(refetch=False, max_connections=15, req_delay=0.1, semaphore_connections=5)
+model.prune_products()
+# Fetch product information (from UniprotAPI or EnsemblAPI)
+model.fetch_product_infos(refetch=False, run_async=True, max_connections=15, semaphore_connections=10, req_delay=0.1)
+model.prune_products()
+# Save model
+model.save_model("diabetes_angio_4/model.json")
+
+# Score products (only fisher_test is relevant here)
+goaf = GOAnnotiationsFile()
+# nterms_test = nterms(model)
+# adv_product_score_test = adv_product_score(model)
+fisher_test = fisher_exact_test(model, goaf)
+# binom_test = binomial_test(model, goaf)
+# model.score_products(score_classes=[nterms_test, adv_product_score_test, fisher_test, binom_test])
+model.score_products(score_classes=[fisher_test])
+model.save_model("diabetes_angio_4/model_scored.json")
+
+# Perform statistical analysis of the scored products
+model.perform_statistical_analysis(test_name="fisher_test", filepath="diabetes_angio_4/results.json")
+model.save_model("diabetes_angio_4/model_analysed.json")
